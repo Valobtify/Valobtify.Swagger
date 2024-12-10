5d62eb7831c43f8ab493252e2b2d07571c72980b
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-12-10 22:10:07 +0330

    chore: add xml comments

--- Valobtify.Swagger/Extensions.cs
+++ Valobtify.Swagger/Extensions.cs
@@ -3,8 +3,16 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Valobtify.Swagger;
 
+/// <summary>
+/// Provides extension methods for configuring Swagger schema filters.
+/// </summary>
 public static class Extensions
 {
+    /// <summary>
+    /// Adds schema filters for Valobtify types to the Swagger configuration.
+    /// </summary>
+    /// <param name="options">The SwaggerGenOptions to configure.</param>
+    /// <returns>The configured SwaggerGenOptions.</returns>
     public static SwaggerGenOptions AddValobtifySchemaFilters(this SwaggerGenOptions options)
     {
         options.SchemaFilter<SingleValueObjectSchemaFilter>();
--- Valobtify.Swagger/SingleValueObjectSchemaFilter.cs
+++ Valobtify.Swagger/SingleValueObjectSchemaFilter.cs
@@ -3,10 +3,18 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Valobtify.Swagger;
 
+/// <summary>
+/// A schema filter for handling single value objects in Swagger documentation.
+/// </summary>
 public class SingleValueObjectSchemaFilter : ISchemaFilter
 {
     private static readonly SingleValueObjectTemplate Template = SingleValueObjectTemplate.Create("")!;
 
+    /// <summary>
+    /// Applies custom schema modifications for single value objects in the Swagger documentation.
+    /// </summary>
+    /// <param name="schema">The OpenAPI schema to modify.</param>
+    /// <param name="context">The context containing information about the schema being generated.</param>
     public void Apply(OpenApiSchema schema, SchemaFilterContext context)
     {
         if (!context.Type.IsSingleValueObject()) return;
